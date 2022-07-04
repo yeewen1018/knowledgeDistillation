ee31e99c31333389e0554c1ea2111f3490a12289
yeewen1018 <57508556+yeewen1018@users.noreply.github.com>
2022-07-04 10:14:28 +0800

    Made sure that models are in evaluation mode.

--- specialist_kd_ensemble/ensemble.py
+++ specialist_kd_ensemble/ensemble.py
@@ -5,11 +5,11 @@ import torch.nn.functional as F
 
 import numpy as np 
 
-def get_predicted_class(num_classes, baseline_class, generalist_output, specialist_outputs, sub_classes): 
+
+def get_predicted_class(num_classes, baseline_class, generalist_output, specialist_outputs, sub_classes, device): 
 
     q_dist = torch.randn(num_classes, requires_grad = True)
-    if torch.cuda.is_available(): 
-        q_dist = q_dist.cuda() 
+    q_dist.to(device)
 
     optimizer = optim.Adam([q_dist], lr = 0.01)
     criterion = nn.KLDivLoss() 
@@ -28,8 +28,7 @@ def get_predicted_class(num_classes, baseline_class, generalist_output, speciali
                 
             modified_q_dist[len(sub_class)] = torch.sum(q_dist_copy)
             modified_q_dist = torch.tensor(modified_q_dist)
-            if torch.cuda.is_available(): 
-                modified_q_dist = modified_q_dist.cuda()
+            modified_q_dist = modified_q_dist.to(device)
             loss += criterion(F.log_softmax(modified_q_dist), F.softmax(specialist_outputs[i]))
     
     #while loss > 1e-3: 
@@ -50,8 +49,7 @@ def get_predicted_class(num_classes, baseline_class, generalist_output, speciali
                     q_dist_copy[sub_class[index]] = 0.0 
                 modified_q_dist[len(sub_class)] = torch.sum(q_dist_copy)
                 modified_q_dist = torch.tensor(modified_q_dist)
-                if torch.cuda.is_available(): 
-                    modified_q_dist = modified_q_dist.cuda()  
+                modified_q_dist.to(device)
                 loss += criterion(F.log_softmax(modified_q_dist), F.softmax(specialist_outputs[i]))
             
     _, predicted = torch.max(q_dist.data, 0)
@@ -61,13 +59,19 @@ def get_predicted_class(num_classes, baseline_class, generalist_output, speciali
 def run_iterative_optimisation(opt, generalist_model, specialist_models, sub_classes, testloader):
     """ index_corrects contains the distribution of the ensemble for every example, and information 
     about whether if the ensemble got the correct prediction (correct = 1, wrong = 0). """
-    num_classes = opt.num_classes
+    
+    # Make sure models are in evaluation model
+    generalist_model.eval() 
+    for specialist_i in range(opt.num_specialists): 
+        specialist_model =specialist_models[specialist_i]
+        specialist_model.eval() 
+    
+    # RUn iterative algorithm on all examples
     corrects, total, index = 0, 0, 0  
     index_corrects = [] 
-    for i, data in enumerate(testloader): 
-        inputs, labels = data 
-        if torch.cuda.is_available(): 
-            inputs, labels = inputs.cuda(), labels.cuda() 
+    for inputs, labels in testloader: 
+        inputs, labels = inputs.to(opt.device), labels.to(opt.device)
+        
         num_examples_per_batch = labels.shape[0]
         for example_i in range(num_examples_per_batch): 
             generalist_output = generalist_model(inputs)
@@ -78,14 +82,12 @@ def run_iterative_optimisation(opt, generalist_model, specialist_models, sub_cla
                 specialist_out = specialist_model(inputs)
                 specialist_outputs.append(specialist_out)
 
-            ensemble_predicted, q_dist = get_predicted_class(num_classes, baseline_class, generalist_output, specialist_outputs, sub_classes)
+            ensemble_predicted, q_dist = get_predicted_class(opt.num_classes, baseline_class, generalist_output, specialist_outputs, sub_classes, opt.device)
             if ensemble_predicted == labels[example_i]: 
-                corrects += 1
+                corrects += 1 
                 index_corrects.append([index, q_dist, 1])
             else: 
                 index_corrects.append([index, q_dist, 0])
-            total += 1 
-            index += 1 
+            total += 1
+            index += 1
     return corrects, total, index_corrects 
-
-    
